b360a65301a93af03839ce9ee89a3f2bd68e0daf
sebjwallace <sebjwallace@gmail.com>
2019-10-28 19:51:17 +0000

    completed box canvas controls

--- src/modules/image-anotation/components/BoxCanvas.tsx
+++ src/modules/image-anotation/components/BoxCanvas.tsx
@@ -3,9 +3,9 @@ import './BoxCanvas.scss'
 
 type BoxCanvasProps = {
   imagePath: string,
-  scale: number
+  scale: number,
+  handleRadius?: number
 }
-
 class BoxCanvas extends React.Component<BoxCanvasProps> {
 
   private $canvas: any
@@ -48,32 +48,132 @@ class BoxCanvas extends React.Component<BoxCanvasProps> {
     ctx.strokeRect(x, y, width, height)
   }
 
-  onMouseMove = (
-    { nativeEvent: event }: { nativeEvent: MouseEvent }
-  ) => {
-    if(!this.state.isMouseDown) return
-    const { offsetX, offsetY } = event
-    const width = offsetX - this.state.boxPos.x
-    const height = offsetY - this.state.boxPos.y
-    this.setState({ boxDims: { width, height } })
-  }
-
   onMouseDown = ({ nativeEvent: event }: { nativeEvent: MouseEvent }) => {
+    this.drag = this.dragBottomRight
     this.setState({
       boxPos: { x: event.offsetX, y: event.offsetY },
+      boxDims: { width: 0, height: 0 },
       isMouseDown: true
     })
+  }
+
+  onMouseMove = (event : any) => {
+    const {
+      boxPos = this.state.boxPos,
+      boxDims = this.state.boxDims
+    } = this.drag(event) || this.state
+    
+    const isValid = boxDims.width >= 0 && boxDims.height >= 0
+    if(this.state.isMouseDown && isValid){
+      this.setState({ boxPos, boxDims })
+    }
   }
 
   onMouseUp = () => {
     this.setState({ isMouseDown: false })
   }
 
+  enableDrag = (dragMode: Function) => {
+    this.setState({ isMouseDown: true })
+    this.drag = dragMode
+  }
+
+  drag: any = () => {}
+
+  dragTopLeft = ({ movementX, movementY } : any) => ({
+    boxPos: {
+      x: this.state.boxPos.x + movementX,
+      y: this.state.boxPos.y + movementY
+    },
+    boxDims: {
+      width: this.state.boxDims.width - movementX,
+      height: this.state.boxDims.height - movementY
+    }
+  })
+
+  dragBottomRight = ({ movementX, movementY } : any) => ({
+    boxDims: {
+      width: this.state.boxDims.width + movementX,
+      height: this.state.boxDims.height + movementY
+    }
+  })
+
+  dragBottomLeft = ({ movementX, movementY } : any) => ({
+    boxPos: {
+      x: this.state.boxPos.x + movementX,
+      y: this.state.boxPos.y
+    },
+    boxDims: {
+      width: this.state.boxDims.width - movementX,
+      height: this.state.boxDims.height + movementY
+    }
+  })
+
+  dragTopRight = ({ movementX, movementY } : any) => ({
+    boxPos: {
+      x: this.state.boxPos.x,
+      y: this.state.boxPos.y + movementY
+    },
+    boxDims: {
+      width: this.state.boxDims.width + movementX,
+      height: this.state.boxDims.height - movementY
+    }
+  })
+
+  dragAll = ({ movementX, movementY } : any) => ({
+    boxPos: {
+      x: this.state.boxPos.x + movementX,
+      y: this.state.boxPos.y + movementY
+    }
+  })
+
   render(){
 
-    const { imagePath, scale } = this.props
+    const { imagePath, scale, handleRadius = 5 } = this.props
+    const { boxPos, boxDims } = this.state
     const width = (this.$image || {}).naturalWidth * scale || 0
     const height = (this.$image || {}).naturalHeight * scale || 0
+
+    const handles = [
+      {
+        style: {
+          left: boxPos.x,
+          top: boxPos.y,
+          width: boxDims.width,
+          height: boxDims.height
+        },
+        className: "box",
+        handler: this.dragAll
+      },
+      {
+        style: {
+          left: boxPos.x - handleRadius,
+          top: boxPos.y - handleRadius
+        },
+        handler: this.dragTopLeft
+      },
+      {
+        style: {
+          left: boxPos.x + boxDims.width - handleRadius,
+          top: boxPos.y + boxDims.height - handleRadius
+        },
+        handler: this.dragBottomRight
+      },
+      {
+        style: {
+          left: boxPos.x - handleRadius,
+          top: boxPos.y + boxDims.height - handleRadius
+        },
+        handler: this.dragBottomLeft
+      },
+      {
+        style: {
+          left: boxPos.x + boxDims.width - handleRadius,
+          top: boxPos.y - handleRadius
+        },
+        handler: this.dragTopRight
+      }
+    ]
 
     return <div className="BoxCanvas">
       <img
@@ -89,10 +189,20 @@ class BoxCanvas extends React.Component<BoxCanvasProps> {
         className="canvas"
         width={width}
         height={height}
-        onMouseDown={this.onMouseDown}
         onMouseUp={this.onMouseUp}
+        onMouseDown={this.onMouseDown}
         onMouseMove={this.onMouseMove}
       />
+      {
+        handles.map(({ style, handler, className } : any, key: number) => <div
+          key={key}
+          className={ className || "cursor" }
+          style={style}
+          onMouseUp={this.onMouseUp}
+          onMouseDown={() => this.enableDrag(handler)}
+          onMouseMove={this.onMouseMove}
+        />)
+      }
     </div>
 
   }
